0250ec8ea9ee3335b8eeb89c89a6468a3b6874e6
Antti <racktapp@gmail.com>
2025-07-01 08:23:18 +0000

    dont make own logos!!! please use my-logo2.png as the apps logo

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -1,20 +1,20 @@
 import * as React from 'react';
-import {cn} from '@/lib/utils';
+import Image from 'next/image';
+import { cn } from '@/lib/utils';
 
-export const Logo = React.forwardRef<SVGSVGElement, React.SVGProps<SVGSVGElement>>(
-  ({className, ...props}, ref) => (
-    <svg
-      ref={ref}
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="currentColor"
-      className={cn('h-7 w-7 text-primary', className)}
-      {...props}
-    >
-      <title>Rackt Logo</title>
-      <path d="M12 2a10 10 0 1 0 10 10A10 10 0 0 0 12 2zm0 18a8 8 0 1 1 8-8 8 8 0 0 1-8 8z" />
-      <path d="M4.2 16.9a8 8 0 0 1 11.2-11.2" />
-    </svg>
-  )
-);
+export const Logo = React.forwardRef<
+  HTMLImageElement,
+  Omit<React.ComponentProps<typeof Image>, 'src' | 'alt'>
+>(({ className, ...props }, ref) => (
+  <Image
+    ref={ref}
+    src="/my-logo2.png"
+    alt="Rackt Logo"
+    width={28}
+    height={28}
+    className={cn('h-7 w-7', className)}
+    unoptimized
+    {...props}
+  />
+));
 Logo.displayName = 'Logo';
