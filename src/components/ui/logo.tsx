672811ba5bd0e98d71563dd1d6329c1b2d329cb9
Antti <racktapp@gmail.com>
2025-07-01 08:16:11 +0000

    delete everything about that, we are going to make my logo work

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -2,24 +2,15 @@ import * as React from "react"
 import { cn } from "@/lib/utils"
 
 export const Logo = React.forwardRef<
-  SVGSVGElement,
-  React.SVGProps<SVGSVGElement>
+  HTMLImageElement,
+  React.ImgHTMLAttributes<HTMLImageElement>
 >(({ className, ...props }, ref) => (
-  <svg
+  <img
     ref={ref}
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    strokeWidth="1.5"
-    strokeLinecap="round"
-    strokeLinejoin="round"
+    src="/my-logo.svg"
+    alt="Rackt Logo"
     className={cn("h-7 w-7", className)}
     {...props}
-  >
-    <circle cx="12" cy="12" r="10" />
-    <path d="M4.2 8.8a10.3 10.3 0 0 1 15.6 0" />
-    <path d="M4.2 15.2a10.3 10.3 0 0 0 15.6 0" />
-  </svg>
+  />
 ));
 Logo.displayName = "Logo";
