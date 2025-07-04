5f72ad573fecc918b91a6f0c16f442a0fb462321
Antti <racktapp@gmail.com>
2025-07-04 20:23:48 +0000

    the apps logo is my-logo1. the tennis_icon is the loading icon and the i

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -8,7 +8,7 @@ export const Logo = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <Image
     ref={ref}
-    src="/tennis_icon.png"
+    src="/my-logo1.png"
     alt="Rackt Logo"
     width={28}
     height={28}
