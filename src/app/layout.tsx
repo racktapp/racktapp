7b7f560eff231bdb7778f71e4a26bc435b83abe7
Antti <racktapp@gmail.com>
2025-07-08 16:19:46 +0000

    this is a test of this app on an iphone. As you can see it doesnt work o

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,10 @@ export const viewport: Viewport = {
     { media: '(prefers-color-scheme: light)', color: 'white' },
     { media: '(prefers-color-scheme: dark)', color: 'black' },
   ],
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
 };
 
 
