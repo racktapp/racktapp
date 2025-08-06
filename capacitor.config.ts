2e8a57affd6b365b7a40fd4c7743e7891962eb3c
Antti <racktapp@gmail.com>
2025-08-06 19:44:35 +0000

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,11 +1,6 @@
 export default {
   appId: 'com.taivas.rackt',    // must match your Android applicationId
   appName: 'RacktApp',          // your app’s display name
-  webDir: '.next',
-  bundledWebRuntime: false,
-  ios: {
-    config: {
-      ITSAppUsesNonExemptEncryption: false
-    }
-  }
+  webDir: '.next',                 // **.next**, since you’re not running `npm run export`
+  bundledWebRuntime: false
 };
