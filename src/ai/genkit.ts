6fa99fc5908205db6e090cd28b3f32bb3395d933
Antti <racktapp@gmail.com>
2025-08-07 17:25:46 +0000

    still

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,5 +2,4 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
-  plugins: [googleAI({apiKey: process.env.GEMINI_API_KEY})],
-});
+  plugins: [googleAI({apiKey: 'AIzaSyAcSm8ZhLI6epx4zbRYvJZNJGHEJdTaLdY'})
