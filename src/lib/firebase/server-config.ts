76b8e81cd3fcb2e87989aae9175bb4590cb732bf
Antti <racktapp@gmail.com>
2025-08-07 15:44:00 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase/server-config.ts
+++ src/lib/firebase/server-config.ts
@@ -15,7 +15,7 @@ function getServiceAccount() {
 const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT 
   ? getServiceAccount() 
   : {
-      "project_id": process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+      "project_id": process.env.FIREBASE_PROJECT_ID,
       "private_key": process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
       "client_email": process.env.FIREBASE_CLIENT_EMAIL,
     };
