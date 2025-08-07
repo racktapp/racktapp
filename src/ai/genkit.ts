c258940b18589e424ceefcebdcfb4162bf764508
Antti <racktapp@gmail.com>
2025-08-07 17:50:50 +0000

    stilllllllllllllllll

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,8 +1,5 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
-import * as dotenv from 'dotenv';
-
-dotenv.config();
 
 export const ai = genkit({
   plugins: [googleAI({apiKey: process.env.GEMINI_API_KEY})],
