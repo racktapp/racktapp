0f0c85ff4b5c308bcf4f2d74443e4a876707b536
Antti <racktapp@gmail.com>
2025-08-11 08:07:36 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/(app)/profile/[id]/page.tsx
+++ src/app/(app)/profile/[id]/page.tsx
@@ -2,6 +2,12 @@
 'use client';
 import ProfileClientView from './ClientView';
 
+export const dynamicParams = false;
+
+export function generateStaticParams() {
+  return [];
+}
+
 export default function Page() {
     return <ProfileClientView />
 }
