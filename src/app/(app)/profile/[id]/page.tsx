cf4f281e24af31cde60cb8abf8519a99b5e2486f
Antti <racktapp@gmail.com>
2025-08-11 08:06:37 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/(app)/profile/[id]/page.tsx
+++ src/app/(app)/profile/[id]/page.tsx
@@ -1,10 +1,7 @@
 
+'use client';
 import ProfileClientView from './ClientView';
 
 export default function Page() {
     return <ProfileClientView />
-}
-
-export function generateStaticParams() {
-    return [{ id: 'placeholder' }];
 }
